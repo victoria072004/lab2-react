102b83d1c13395ae19c2ac17b94eb8fb56a04e28
Turcan Victoria <victoria1turcan@gmail.com>
2025-03-01 09:38:34 +0200

    Rezolvat lab 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import Game from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Game />
   </StrictMode>
 );
